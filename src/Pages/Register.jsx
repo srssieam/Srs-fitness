de82a86c92e0adcc7f1ce85b189c81e3e06441ed
srs-sieam <srssieam@gmail.com>
2023-10-08 11:24:21 +0600

    made login and register page is responsive now

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -20,37 +20,37 @@ const Register = () => {
     return (
         <div>
             <div style={regiStyle}>
-            <div className='flex justify-between items-center h-[100%] w-full py-10'>
-                <div className='bg-transparent w-3/5 h-full flex flex-col gap-9 justify-around px-20 text-white'>
+            <div className='md:flex justify-between items-center h-[100%] w-full py-10'>
+                <div className='bg-transparent w-3/5 h-full flex flex-col md:gap-9 justify-around px-5 md:px-8 lg:px-20 text-white'>
                     <div>
-                        <h1 className='text-6xl font-semibold'>Welcome !</h1>
-                        <p className='text-4xl font-semibold py-6'>To Srs Fitness</p>
+                        <h1 className='text-2xl md:text-4xl lg:text-6xl font-semibold'>Welcome !</h1>
+                        <p className='text-xl md:text-2 lg:text-4xl font-semibold md:py-3 lg:py-6'>To Srs Fitness</p>
                     </div>
                     <div>
-                        <h1 className='text-5xl font-semibold'>Wellness Starts Here.</h1>
-                        <h3 className='text-3xl font-semibold py-5'>We are committed to <br /> bringing happiness into your life.</h3>
+                        <h1 className='text-xl md:text-3xl lg:text-5xl font-semibold'>Wellness Starts Here.</h1>
+                        <h3 className='text:lg md:xl lg:text-3xl font-semibold py-5'>We are committed to <br /> bringing happiness into your life.</h3>
                     </div>
                 </div>
-                <div className='w-2/5 h-full flex items-center bg-transparent backdrop-blur-xl p-10 mr-10 rounded-3xl'>
+                <div className='md:w-2/5 h-full flex items-center bg-transparent backdrop-blur-xl  p-4 md:p-6 lg:p-11 md:mr-5 lg:mr-10 rounded-3xl'>
                     <div className='w-full'>
-                    <h1 className='text-4xl font-semibold py-7 text-white'>Register</h1>
-                    <form onSubmit={handleRegister} className=' space-y-8'>
+                    <h1 className='text-xl md:text-2xl lg:text-4xl font-semibold py-2 md:py-3 lg:py-7 text-white'>Register</h1>
+                    <form onSubmit={handleRegister} className='md:space-y-3 lg:space-y-8'>
                         <div>
-                            <label htmlFor="name" className=' text-white font-semibold text-2xl'>Your Name : </label><br />
+                            <label htmlFor="name" className=' text-white font-semibold text-lg lg:text-2xl'>Your Name : </label><br />
                             <div className='flex items-center border-b-2 border-cyan-500'>
                                 <input type="text" name="name" id="name" className='outline-none bg-transparent w-full px-2 py-1' />
                                 <FaUserAlt className='text-2xl text-cyan-500'></FaUserAlt>
                             </div>
                         </div>
                         <div>
-                            <label htmlFor="email" className=' text-white font-semibold text-2xl'>Your Email : </label><br />
+                            <label htmlFor="email" className=' text-white font-semibold text-lg lg:text-2xl'>Your Email : </label><br />
                             <div className='flex items-center border-b-2 border-cyan-500'>
                                 <input type="email" name="email" id="email" className='outline-none bg-transparent w-full px-2 py-1' required />
                                 <MdEmail className='text-2xl text-cyan-500'></MdEmail>
                             </div>
                         </div>
                         <div>
-                            <label htmlFor="password" className=' text-white font-semibold text-2xl'>Your Password : </label><br />
+                            <label htmlFor="password" className=' text-white font-semibold text-lg lg:text-2xl'>Your Password : </label><br />
                             <div className='flex items-center border-b-2 border-cyan-500'>
                                 <input type="password" name="password" id="password" className='outline-none bg-transparent w-full px-2 py-1' required />
                                 <FaLock className='text-2xl text-cyan-500'></FaLock>
