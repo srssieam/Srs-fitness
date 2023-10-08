61e0deb8b8ce4bc214cd16e3ee7880a6a8971842
srs-sieam <srssieam@gmail.com>
2023-10-08 17:40:49 +0600

    used onAuthStateChanged to observe current user

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,11 +1,12 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import app from "../firebase.config";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
 
     const createUser = (email, password)=>{
        return createUserWithEmailAndPassword(auth, email, password);
@@ -14,7 +15,19 @@ const AuthProvider = ({children}) => {
     const userLogin = (email, password)=>{
         return signInWithEmailAndPassword(auth, email, password);
     }
-    const authInfo={createUser, userLogin};
+
+    // observe onAuthStateChanged
+    useEffect(()=>{
+        const unSubscribe = onAuthStateChanged(auth, (currentUser)=>{
+            setUser(currentUser)
+            console.log('observing current user', currentUser)
+        });
+        return()=>{
+            unSubscribe();
+        }
+    },[])
+
+    const authInfo={createUser, userLogin, user};
     return (
         <div>
             <AuthContext.Provider value={authInfo}>
