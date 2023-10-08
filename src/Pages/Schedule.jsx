4171bb08a1274d99add27845ac6e669d015e7260
srs-sieam <srssieam@gmail.com>
2023-10-08 13:03:04 +0600

    created Schedule page

--- src/Pages/Schedule.jsx
+++ src/Pages/Schedule.jsx
@@ -2,8 +2,64 @@
 
 const Schedule = () => {
     return (
-        <div>
-            
+        <div className="py-4 md:py-16 px-6 lg:px-0">
+            <h1 className="text-center text-2xl md:text-3xl lg:text-5xl  font-bold">Harmony in Seven Days: <br /> A Health and Wellness Blueprint</h1>
+            <div>
+                <h1 className="font-bold text-xl md:text-2xl">Monday:</h1>
+                <ul className="mt-4 ml-6">
+                    <li className="md:text-xl list-disc"><span className="font-bold">Morning:</span> Yoga session (30 minutes to 1 hour)</li>
+                    <li className="md:text-xl list-disc"><span className="font-bold">Afternoon:</span> Counseling or therapy session (1 hour)</li>
+                    <li className="md:text-xl list-disc"><span className="font-bold">Evening:</span> Light workout (e.g., walking, cycling, or swimming for 30 minutes) followed by a balanced dinner</li>
+                </ul>
+            </div>
+            <div className="mt-4 lg:mt-8">
+                <h1 className="font-bold text-xl md:text-2xl">Tuesday:</h1>
+                <ul className="mt-4 ml-6">
+                    <li className="md:text-xl list-disc"><span className="font-bold">Morning:</span>  Strength training or cardio workout (45 minutes to 1 hour)</li>
+                    <li className="md:text-xl list-disc"><span className="font-bold">Afternoon:</span> Healthy lunch with a focus on balanced nutrition</li>
+                    <li className="md:text-xl list-disc"><span className="font-bold">Evening:</span> Relaxing massage therapy session (1 hour)</li>
+                </ul>
+            </div>
+            <div className="mt-4 lg:mt-8">
+                <h1 className="font-bold text-xl md:text-2xl">Wednesday:</h1>
+                <ul className="mt-4 ml-6">
+                    <li className="md:text-xl list-disc"><span className="font-bold">Morning:</span>  Yoga session (30 minutes to 1 hour)</li>
+                    <li className="md:text-xl list-disc"><span className="font-bold">Afternoon:</span> Counseling or therapy session (1 hour)</li>
+                    <li className="md:text-xl list-disc"><span className="font-bold">Evening:</span> Meal prep for the rest of the week, focusing on whole foods and portion control</li>
+                </ul>
+            </div>
+            <div className="mt-4 lg:mt-8">
+                <h1 className="font-bold text-xl md:text-2xl">Thursday:</h1>
+                <ul className="mt-4 ml-6">
+                    <li className="md:text-xl list-disc"><span className="font-bold">Morning:</span> Strength training or cardio workout (45 minutes to 1 hour)</li>
+                    <li className="md:text-xl list-disc"><span className="font-bold">Afternoon:</span> Healthy lunch with a focus on balanced nutrition</li>
+                    <li className="md:text-xl list-disc"><span className="font-bold">Evening:</span> Attend a group counseling or support group session if available</li>
+                </ul>
+            </div>
+            <div className="mt-4 lg:mt-8">
+                <h1 className="font-bold text-xl md:text-2xl">Friday:</h1>
+                <ul className="mt-4 ml-6">
+                    <li className="md:text-xl list-disc"><span className="font-bold">Morning:</span> Yoga session (30 minutes to 1 hour)</li>
+                    <li className="md:text-xl list-disc"><span className="font-bold">Afternoon:</span> Counseling or therapy session (1 hour)</li>
+                    <li className="md:text-xl list-disc"><span className="font-bold">Evening:</span> Relaxation time (read, meditate, or engage in a hobby)</li>
+                </ul>
+            </div>
+            <div className="mt-4 lg:mt-8">
+                <h1 className="font-bold text-xl md:text-2xl">Saturday:</h1>
+                <ul className="mt-4 ml-6">
+                    <li className="md:text-xl list-disc"><span className="font-bold">Morning:</span> Outdoor physical activity (e.g., hiking, biking, or a long walk)</li>
+                    <li className="md:text-xl list-disc"><span className="font-bold">Afternoon:</span> Healthy lunch</li>
+                    <li className="md:text-xl list-disc"><span className="font-bold">Evening:</span> Cooking a nutritious dinner at home or trying a new healthy recipe</li>
+                </ul>
+            </div>
+            <div className="mt-4 lg:mt-8">
+                <h1 className="font-bold text-xl md:text-2xl">Sunday:</h1>
+                <ul className="mt-4 ml-6">
+                    <li className="md:text-xl list-disc"><span className="font-bold">Morning:</span> Rest and relaxation (sleep in, leisurely breakfast)</li>
+                    <li className="md:text-xl list-disc"><span className="font-bold">Afternoon:</span>  Free time for personal interests or hobbies</li>
+                    <li className="md:text-xl list-disc"><span className="font-bold">Evening:</span> Reflect on the week and set goals for the upcoming week</li>
+                </ul>
+            </div>
         </div>
     );
 };
