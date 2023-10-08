cdf070fe45ef92da2a42e4411eb1694c39123cae
srs-sieam <srssieam@gmail.com>
2023-10-08 15:18:05 +0600

    added functionality for your programs section

--- src/Pages/Programs.jsx
+++ src/Pages/Programs.jsx
@@ -1,9 +1,45 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const Programs = () => {
+    const [addedPackages, setAddedPackages] = useState([]);
+    const [noPackagesFound, setNoPackagesFound] = useState();
+
+    useEffect(() => {
+        const programs = JSON.parse(localStorage.getItem('Programs'));
+        if (programs) {
+            setAddedPackages(programs)
+        } else {
+            setNoPackagesFound('You have not enrolled any packages yes !!')
+        }
+    }, [])
+    console.log(addedPackages)
     return (
         <div>
-            
+            {
+                addedPackages.length > 0 ?
+                    <div className="h-[80vh] p-3 md:p-12">
+                        <p className="text-xl md:text-3xl font-semibold">Total programs to be completed: {addedPackages.length}</p>
+                        {
+                            addedPackages?.map(program => {
+                                return (
+                                    <div key={program.id} className="flex justify-between w-full border-b font-semibold border-cyan-500 md:p-3 mb-4">
+                                        <li className=" md:text-2xl text-cyan-600 list-disc">{program.title}</li>
+                                        <p className="text-sm md:text-base">Pending</p>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                    :
+                    <div className="h-[80vh] p-6 md:p-12 flex justify-center items-center">
+                        <div className="text-center">
+                            <h1 className="text-3xl md:text-6xl font-semibold text-cyan-600">{noPackagesFound}</h1>
+                            <p className="text-xl md:text-3xl mt-6">Let's inspact our <Link to='/service' className="text-cyan-600 font-semibold hover:underline">services</Link>.</p>
+                        </div>
+                    </div>
+            }
         </div>
     );
 };
--- src/Pages/ServiceDetails.jsx
+++ src/Pages/ServiceDetails.jsx
@@ -18,47 +18,71 @@ const ServiceDetails = () => {
     }, [id, loadedData])
     // console.log(serviceData)
 
-    const { title, image, details, Price } = serviceData
-    console.log(title)
+    const { title, image, details, Price } = serviceData;
 
-    const handleEnroll =()=>{
+
+    const handleEnroll = () => {
+
+        // sweet alerts
         const swalWithBootstrapButtons = Swal.mixin({
             customClass: {
-              confirmButton: 'btn btn-success',
-              cancelButton: 'btn btn-danger'
+                confirmButton: 'btn btn-success',
+                cancelButton: 'btn btn-danger'
             },
             buttonsStyling: false
-          })
-          
-          swalWithBootstrapButtons.fire({
+        })
+
+        swalWithBootstrapButtons.fire({
             title: `This package will cost ${Price} $`,
-            text: `Are you sure !` ,
+            text: `Are you sure !`,
             icon: 'warning',
             showCancelButton: true,
             cancelButtonText: 'No, cancel!',
             confirmButtonText: 'Yes, Proceed!',
             reverseButtons: true
-          }).then((result) => {
+        }).then((result) => {
             if (result.isConfirmed) {
-              swalWithBootstrapButtons.fire(
-                'Successful!',
-                'Thank you for choosing to enroll in our course!',
-                'success'
-              )
+                swalWithBootstrapButtons.fire(
+                    'Successful!',
+                    'Thank you for choosing to enroll in our course!',
+                    'success'
+                )
             } else if (
-              /* Read more about handling dismissals below */
-              result.dismiss === Swal.DismissReason.cancel
+                /* Read more about handling dismissals below */
+                result.dismiss === Swal.DismissReason.cancel
             ) {
-              swalWithBootstrapButtons.fire(
-                'Cancelled',
-                'We appreciate your decision :)',
-                'error'
-              )
+                swalWithBootstrapButtons.fire(
+                    'Cancelled',
+                    'We appreciate your decision :)',
+                    'error'
+                )
             }
-          })
+        })
+
+        
+
+        // save enrolled packages to localStorage
+        const enrolledCoursesArray = [];
+        const enrolledCourses = JSON.parse(localStorage.getItem('Programs'));
+        if (!enrolledCourses) {
+            enrolledCoursesArray.push(serviceData);
+            localStorage.setItem('Programs', JSON.stringify(enrolledCoursesArray));
+        } else {
+            const isExist = enrolledCourses.find(corse => corse.id === serviceData.id) // step 7
+            if (!isExist) {
+                enrolledCoursesArray.push(...enrolledCourses, serviceData) // step 8
+                localStorage.setItem('Programs', JSON.stringify(enrolledCoursesArray))  // step 9
+            } else {
+                Swal.fire({
+                    icon: 'warning',
+                    title: 'Oops...',
+                    text: 'You have already enrolled this package',
+                })
+            }
+        }
     }
 
-    
+
     return (
         <div>
             <div className="relative flex w-full flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
@@ -74,10 +98,10 @@ const ServiceDetails = () => {
                             {title}
                         </p>
                         <button onClick={handleEnroll}
-                        className=" select-none rounded-lg bg-slate-900 text-white py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                        type="button">
-                        enroll now
-                    </button>
+                            className=" select-none rounded-lg bg-slate-900 text-white py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                            type="button">
+                            enroll now
+                        </button>
                     </div>
                     <p className="block text-justify font-sans text-sm md:text-xl font-normal leading-normal text-gray-700 antialiased opacity-75">
                         {details}
