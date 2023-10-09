7c5b6274b5c7da5b462d6e3d4a7357196bcf9b17
srs-sieam <srssieam@gmail.com>
2023-10-09 07:29:53 +0600

    dynamic title

--- src/MainLayout/Root.jsx
+++ src/MainLayout/Root.jsx
@@ -1,9 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../components/Header/Navbar";
 import Footer from "../components/Footer/Footer";
+import { useEffect } from "react";
 
 
 const Root = () => {
+    const loc = useLocation();
+    useEffect(()=>{
+        document.title = loc.pathname;
+        document.title = `SrsFitness ${loc.pathname.replace("/","-")}`;
+    },[loc.pathname])
     return (
         <div className="max-w-[1300px] mx-auto">
             <Navbar></Navbar>
